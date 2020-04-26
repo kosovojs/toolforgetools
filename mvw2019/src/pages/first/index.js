243e53cf5a1a77b894481986b42ea3a140eb56b0
edgarskos <kosovojs@gmail.com>
2020-04-26 15:11:05 +0300

    upd mvw from server

--- mvw2019/src/pages/first/index.js
+++ mvw2019/src/pages/first/index.js
@@ -2,7 +2,7 @@ require("../../main");
 
 import { handleVoteSave, updateUserCount, voteButtonElement, votingButtons } from "../../main";
 
-//voteButtonElement.addEventListener("click", () => handleVoteSave("first"));
+voteButtonElement.addEventListener("click", () => handleVoteSave("first"));
 
 window.addEventListener("load", function() {
 	updateUserCount(15);
